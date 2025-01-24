22e4f331dc7750a5eca9f42f6c0939f135aa5f34
Sumir Seth <iamsumirseth@gmail.com>
2025-01-24 20:32:56 +0530

    initialize Nuxt.js application with TypeScript, Tailwind CSS, and basic configuration

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <Link rel="icon" href="/favicon.png" />
+  <Title>Shortest Path</Title>
+  <Meta name="description" content="Shortest Path" />
+  <div class="font-[Outfit] text-5xl font-black text-center p-5 text-white bg-black min-h-screen">
+    Hello Path Finder
+  </div>
+</template>
